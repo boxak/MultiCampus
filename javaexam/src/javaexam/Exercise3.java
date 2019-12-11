cb3c180ee35775be962efe5aa42209c8752b5d5e
boxak <boxak@naver.com>
2019-12-11 11:00:44 +0900

    Add javaexam

---
+++ javaexam/src/javaexam/Exercise3.java
@@ -0,0 +1,12 @@
+package javaexam;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		int i=20;
+		float f1=20.0f;
+		float result=f1+i;
+		System.out.println(result);
+	}
+
+}
--- javaexam/src/javaexam/exercise2.java
+++ javaexam/src/javaexam/exercise2.java
@@ -17,7 +17,10 @@ public class exercise2 {
 		System.out.printf("d=%14.10f%n", d); //전체 14자리 중 소수점 10자리
 		
 		System.out.printf("[12345678901234567890]%n");
-
+		System.out.printf("[%s]%n", url);
+		System.out.printf("[%20s]%n", url);
+		System.out.printf("[%-20s]%n", url); //왼쪽 정렬
+		System.out.printf("[%.8s]%n", url); //왼쪽에서 8글자만 출력
 	}
 
 }
