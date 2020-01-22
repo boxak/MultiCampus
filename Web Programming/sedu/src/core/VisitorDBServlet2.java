f1bc48240a22308d71e97a6451dcbf47252090ed
boxak <38724041+boxak@users.noreply.github.com>
2020-01-22 14:36:27 +0900

    20200122 Learning
    20200122 Learning

--- Web Programming/sedu/src/core/VisitorDBServlet2.java
+++ Web Programming/sedu/src/core/VisitorDBServlet2.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -15,8 +16,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/visitordb")
-public class VisitorDBServlet extends HttpServlet {
+@WebServlet("/visitordb2")
+public class VisitorDBServlet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	//GET - visitor 테이블의 데이터들을 모두 읽어 와서 테이블 형식으로 브라우저로 출력
@@ -78,11 +79,15 @@ public class VisitorDBServlet extends HttpServlet {
 		}
 		
 		//DB서버 접속, Statement 객체 생성, "SELECT * FROM VISITOR" SQL 명령문 수행
-		String sql = "insert into visitor values('"+name+"',sysdate,'"+memo+"')";
+		//select는 그냥 statement를 쓴다. insert는 preparedstatement를 쓰는 게 더 적합함.
+		//preparedStatement는 객체 생성시 이미 sql문을 생성자 아규먼트로 줘야 한다.
+		String sql = "insert into visitor values(?,sysdate,?)";
 		System.out.println(sql);
-		try (	Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "jdbctest", "jdbctest");
-				Statement stmt = conn.createStatement();) {
-			stmt.executeUpdate(sql);
+		try (Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "jdbctest", "jdbctest");
+				PreparedStatement pstmt = conn.prepareStatement(sql);) {
+			pstmt.setString(1,name);
+			pstmt.setString(2,memo);
+			pstmt.executeUpdate(); //->이 때, sql문을 주면 안된다. 이미 sql문이 준비되어 있기 때문.
 			//executeUpdate는 close가 필요하지 않은 메서드이기 때문에 try with close 구문안에 쓸 수 없다.
 			//여기까지 왔으면 예외 발생없이 잘 실행되었음을 의미함.
 			out.print("<h2>방명록 저장 성공!!</h2>");
