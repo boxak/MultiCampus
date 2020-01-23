5982280a46372adcf317a0ee18c1401f80e42c0f
boxak <38724041+boxak@users.noreply.github.com>
2020-01-23 17:44:57 +0900

    20200123 Learning
    20200123 Learning

--- Web Programming/sedu/src/vo/CountVO.java
+++ Web Programming/sedu/src/vo/CountVO.java
@@ -1,5 +1,7 @@
 package vo;
-
+// xxxVO - Value Object - 값을 보관하는 용도의 객체
+// xxxDAO - Data Access Object - DB 연동기능(JDBC)을 지원하는 객체
+// xxxService(xxxBiz) - Service Object - 이런저런 서비스 로직을 지원하는 객체
 public class CountVO {
 	private int number;
 
