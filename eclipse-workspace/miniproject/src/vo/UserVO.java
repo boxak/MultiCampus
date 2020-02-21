591f5827a54071a64d02d65a1a87907a5e81e5c7
boxak <38724041+boxak@users.noreply.github.com>
2020-02-21 15:22:53 +0900

    20200220
    20200220 Learning

---
+++ eclipse-workspace/miniproject/src/vo/UserVO.java
@@ -0,0 +1,17 @@
+package vo;
+
+public class UserVO {
+	String mem_id;
+	String mem_userid;
+	String mem_password;
+	String mem_username;
+	String mem_email;
+	int mem_age;
+	String sex;
+	String telNumber;
+	String address;
+	String joinDate;
+	int rate;
+	int isEmployer; // 0 : Employee, 1 : Employer
+	int pay;
+}
